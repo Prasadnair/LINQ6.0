1cd6be621220c86368b2301fb9b679ca0c4985c6
Prasadnair <prasadnair44@gmail.com>
2022-08-08 16:52:57 -0400

    LINQ dotNET 6.0 features
    LINQ dotNET 6.0 features

---
+++ LINQIndotNET6/LINQIndotNET6/MinByAndMaxBy/Siblings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQIndotNET6.MinByAndMaxBy
+{
+    public class Siblings
+    {
+        public Siblings(string name, int age)        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ LINQIndotNET6/LINQIndotNET6/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] Names = { "Buck", "Micheal", "Marco", "Andrei","Ken","Paul"};
+
+//new approach
+var nameSublist = Names.Take(2..4);
+
+
+Console.ReadKey();
